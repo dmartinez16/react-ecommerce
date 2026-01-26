74dfa4cb6a9b0d8799ed5522ee82a68828d7f1c6
Dilan Esnehider Martinez Silva <d.martinez@syc.com.co>
2026-01-26 10:51:35 -0500

    Initial commit - Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-red-100">
+      <h1 className="">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
